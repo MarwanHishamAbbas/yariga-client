13c5094787e5152b3cc462d34ca4781518b06552
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2023-03-25 01:44:26 +0200

    adding filter UI

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,45 +1,89 @@
 import { Add } from "@mui/icons-material";
-import { Stack, Typography } from "@mui/material";
-import { CustomButton, PropertyCard } from "components/common";
-import { useNavigate } from "react-router-dom";
 import { useTable } from "@refinedev/core";
-import { Box } from "@mui/system";
+import { Box, Stack, Typography, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useMemo } from "react";
+
+import { PropertyCard, CustomButton } from "../components/common";
 
 const AllProperties = () => {
   const navigate = useNavigate();
 
   const {
     tableQueryResult: { data, isLoading, isError },
+    filters,
+    setFilters,
   } = useTable();
 
   const allProperties = data?.data ?? [];
 
-  if (isLoading) return <Typography>Loading....</Typography>;
-  if (isError) return <Typography>Error!!</Typography>;
+  const currentFilterValues = useMemo(() => {
+    const logicalFilters = filters.flatMap((item) =>
+      "field" in item ? item : []
+    );
+
+    return {
+      title: logicalFilters.find((item) => item.field === "title")?.value || "",
+    };
+  }, [filters]);
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Error...</Typography>;
 
   return (
     <Box>
-      <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Typography fontSize={25} fontWeight={700} color="#11142d">
-          All Properties
-        </Typography>
-        <CustomButton
-          title="Add Property"
-          handleClick={() => navigate("/properties/create")}
-          backgroundColor="#475be8"
-          color="#fcfcfc"
-          icon={<Add />}
-        />
-      </Stack>
       <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
-        {allProperties.map((property) => (
+        <Stack direction="column" width="100%">
+          <Typography fontSize={25} fontWeight={700} color="#11142d">
+            {!allProperties.length
+              ? "There are no properties"
+              : "All Properties"}
+          </Typography>
+          <Box
+            mb={2}
+            mt={3}
+            display="flex"
+            width="84%"
+            justifyContent="space-between"
+            flexWrap="wrap"
+          >
+            <TextField
+              variant="outlined"
+              color="info"
+              placeholder="Search by title"
+              value={currentFilterValues.title}
+              onChange={(e) => {
+                setFilters([
+                  {
+                    field: "title",
+                    operator: "contains",
+                    value: e.currentTarget.value
+                      ? e.currentTarget.value
+                      : undefined,
+                  },
+                ]);
+              }}
+            />
+            <CustomButton
+              title="Add Property"
+              handleClick={() => navigate("/properties/create")}
+              backgroundColor="#475be8"
+              color="#fcfcfc"
+              icon={<Add />}
+            />
+          </Box>
+        </Stack>
+      </Box>
+
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        {allProperties?.map((property) => (
           <PropertyCard
-            key={property.id}
+            key={property._id}
+            id={property._id}
             title={property.title}
             location={property.location}
-            photo={property.photo}
             price={property.price}
-            id={property.id}
+            photo={property.photo}
           />
         ))}
       </Box>
